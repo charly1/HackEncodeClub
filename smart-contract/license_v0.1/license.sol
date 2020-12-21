b5205bb013c6538e1af0b80d7cb23dd56b0133c1
sballmer <stephane.ballmer@spie.com>
2020-12-21 11:26:49 +0100

    add first smart contract and byte code

---
+++ smart-contract/license_v0.1/license.sol
@@ -0,0 +1,29 @@
+pragma solidity >=0.7.0 <0.8.0;
+
+contract license {
+
+	address proprio;
+    
+	constructor () {
+    	proprio = msg.sender;
+	}
+    
+	modifier onlyBy(address _account) {
+    	require(
+        	msg.sender == _account,
+        	"Sender not authorized."
+    	);
+    	// Do not forget the "_;"! It will
+    	// be replaced by the actual function
+    	// body when the modifier is used.
+    	_;
+	}
+    
+	function get_prorio() public view returns (address) {
+    	return proprio;
+	}
+    
+	function set_proprio(address new_proprio) public onlyBy(proprio) {
+    	proprio = new_proprio;
+	}
+}
