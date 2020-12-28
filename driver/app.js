60d352a6b90c3ebc83d6d3fa28663869848b1787
cystu <14922265+cystu@users.noreply.github.com>
2020-12-28 18:23:33 +0100

    driver response server

--- driver/app.js
+++ driver/app.js
@@ -16,9 +16,16 @@ var log_url = local_path+':'+index_port;
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/" + index_file);
 });
-app.get("/src/portis_log.js", function (req, res) {
-    res.sendFile(__dirname + "/src/" + "portis_log.js");
-});
+
+app.get('/check_owner', function (req, res) {  
+response = {  
+       bc_owner:req.query.bc_owner,  
+       local_owner:req.query.local_owner  
+   };  
+   console.log(response);  
+   res.end(JSON.stringify(response));  
+})
+
 app.listen(index_port, function () {
     console.log("Server is running on "+local_path+":"+index_port);
 });
